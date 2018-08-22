20c893f793045de54140acba56ccb2d950fe4baa
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-22 15:16:18 -0500

    Implementing method to give the size of a video file.

--- selection/video.py
+++ selection/video.py
@@ -5,4 +5,21 @@
 
 """video.py: When user selects to encode a video, this will return a video object."""
 
+import os
+
+
+class Video:
+    def __init__(self, path_to_video):
+        self.path_to_video = path_to_video
+
+    # return size of video in MB or GB
+    def file_size(self):
+        video_file_size_in_bytes = os.path.getsize(self.path_to_video)
+        video_file_size_in_megabytes = video_file_size_in_bytes / 1000000.00
+
+        if video_file_size_in_megabytes >= 1000:
+            video_file_size_in_gigabytes = video_file_size_in_megabytes / 1000.00
+            return video_file_size_in_gigabytes
+        else:
+            return video_file_size_in_megabytes
 
