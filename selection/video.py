c28b5909b877eeab444a9b862863adba8b295b2b
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-22 15:27:55 -0500

    changing return of method to have decimal precision as well as the proper name of its value. i.e. megabyte/gigabyte.

--- selection/video.py
+++ selection/video.py
@@ -6,6 +6,7 @@
 """video.py: When user selects to encode a video, this will return a video object."""
 
 import os
+from decimal import Decimal     # used for file_size() method
 
 
 class Video:
@@ -15,15 +16,15 @@ class Video:
     # return size of video in MB or GB
     def file_size(self):
         video_file_size_in_bytes = os.path.getsize(self.path_to_video)
-        video_file_size_in_megabytes = video_file_size_in_bytes / 1000000.00
+        video_file_size_in_megabytes = Decimal(video_file_size_in_bytes / 1000000.00)
 
         if video_file_size_in_megabytes >= 1000:
-            video_file_size_in_gigabytes = video_file_size_in_megabytes / 1000.00
-            return video_file_size_in_gigabytes
+            video_file_size_in_gigabytes = Decimal(video_file_size_in_megabytes / 1000.00)
+            return str(round(video_file_size_in_gigabytes, 2)) + ' gigabytes'
         else:
-            return video_file_size_in_megabytes
+            return str(round(video_file_size_in_megabytes, 2)) + ' megabytes'
 
 
 if __name__ == '__main__':
     test_video_0 = Video('W:\Brandon J. Fletcher\Videos\YouTube\MKBHD\Talking Tech with Elon Musk!.mp4')
-    print(str(test_video_0.file_size()))
+    print(test_video_0.file_size())
