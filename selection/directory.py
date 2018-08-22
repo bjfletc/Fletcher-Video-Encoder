c5d78ffdb52e25ae39959c7892e791ae8660a486
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-22 14:59:03 -0500

    First Commit...

---
+++ selection/directory.py
@@ -0,0 +1,7 @@
+# Author: Brandon J. Fletcher
+# Date  : Wednesday, Aug 22, 2018
+# E-mail: brandonjfletcher@gmail.com
+# GitHub: https://github.com/bjfletc
+
+"""directory.py: When user selects to encode an entire directory, this will return a list of video objects."""
+
---
+++ selection/video.py
@@ -0,0 +1,8 @@
+# Author: Brandon J. Fletcher
+# Date  : Wednesday, Aug 22, 2018
+# E-mail: brandonjfletcher@gmail.com
+# GitHub: https://github.com/bjfletc
+
+"""video.py: When user selects to encode a video, this will return a video object."""
+
+
