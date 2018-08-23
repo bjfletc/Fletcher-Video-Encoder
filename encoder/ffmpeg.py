1abe0c49b203febe293f13b34edb21a2c632fc66
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-23 16:36:48 -0500

    adding some TODOs to work on later. @4:36 pm

---
+++ encoder/ffmpeg.py
@@ -0,0 +1,11 @@
+# Author: Brandon J. Fletcher
+# Created  : Wednesday, Aug 23, 2018
+# E-mail: brandonjfletcher@gmail.com
+# GitHub: https://github.com/bjfletc
+
+"""ffmpeg.py: will be the main method for encoding."""
+
+# TODO(1): learn how to pipe the process of encoding from cmd
+# TODO(2): if elif logic for if it is a directory or a video being encoded
+# TODO(3): figure out how to get the status of the video or directory being encoded
+# TODO(4): create method for output file location
--- selector/splash_screen.py
+++ selector/splash_screen.py
@@ -7,6 +7,8 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename, askdirectory # will need later on
 from selection import directory, video
+# TODO(5): learn how to create a SplashScreen OOP class
+
 
 root = Tk()
 root.geometry('500x500')
@@ -14,6 +16,7 @@ root.iconbitmap('../img/fletcher-family-crest.jpg.ico')
 root.title('Fletcher Video Encoder')
 
 
+# TODO(6): if button is pressed, open a new window for encoding that has the folder name
 # returns directory selected from file dialog
 def directory_button_command():
     path_to_directory = askdirectory()
@@ -21,6 +24,7 @@ def directory_button_command():
     return chosen_directory
 
 
+# TODO(7): if button is pressed, open a new window for encoding that has the video name
 # returns video selected from file dialog
 def video_button_command():
     path_to_video = askopenfilename()
