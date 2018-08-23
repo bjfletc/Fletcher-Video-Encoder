be02b2166286e2052412d67456993ecda34c0a91
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-23 15:35:21 -0500

    created two command methods for each button.

--- selector/splash_screen.py
+++ selector/splash_screen.py
@@ -5,8 +5,8 @@
 
 """splash_screen.py: When the program is run, will be the screen that they see first."""
 from tkinter import *
-# from tkinter.filedialog import askopenfile, askdirectory # will need later on
-
+from tkinter.filedialog import askopenfilename, askdirectory # will need later on
+from selection import directory, video
 
 class SplashScreen(Frame):
     def __init__(self, parent=None):
@@ -16,13 +16,26 @@ class SplashScreen(Frame):
         self.make_widgets()
 
     def make_widgets(self):
-        widget = Button(self, text='Hello frame world!', command=self.message)
-        widget.pack(side=LEFT)
+        directory_button = Button(self, text='Directory', command=self.directory_button_command)
+        directory_button.pack(side=LEFT)
 
-    def message(self):
-        self.data += 1
-        return print('Hello frame world %s!' % self.data)
+        video_button = Button(self, text='Video', command=self.video_button_command)
+        video_button.pack(side=RIGHT)
 
+    # TODO: Create methods for each button to create it's own object type
+    # TODO: Figure out warning from IntelliJ IDEA...
+
+    # returns directory selected from file dialog
+    def directory_button_command(self):
+        path_to_directory = askdirectory()
+        chosen_directory = directory.Directory(path_to_directory)
+        return chosen_directory
+
+    # returns video selected from file dialog
+    def video_button_command(self):
+        path_to_video = askopenfilename()
+        chosen_video = video.Video(path_to_video)
+        return chosen_video
 
 if __name__ == '__main__':
     SplashScreen().mainloop()
