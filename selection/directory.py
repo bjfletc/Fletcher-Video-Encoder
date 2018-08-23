127ae23205a8fedd87e29e8f415e51c57ca8ed15
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-22 22:44:11 -0500

    skeleton code for if a user selects an entire directory to encode

--- selection/directory.py
+++ selection/directory.py
@@ -4,6 +4,20 @@
 # GitHub: https://github.com/bjfletc
 
 """directory.py: When user selects to encode an entire directory, this will return a list of video objects."""
+# from tkinter.filedialog import askdirectory
 
 
-# TODO: create class that can create a list of video objects given a directory to encode.
+class Directory:
+    def __init__(self, path_to_directory):
+        self.path_to_directory = path_to_directory
+
+    def name(self):
+        index_of_end_of_path_to_directory = (self.path_to_directory.rfind('\\')) + 1
+        name_of_directory = self.path_to_directory[index_of_end_of_path_to_directory:]
+        return name_of_directory
+
+
+if __name__ == '__main__':
+    # test if module is ran as a script
+    test_directory_0 = Directory('W:\Brandon J. Fletcher\Videos\YouTube\MKBHD')
+    print(test_directory_0.name())
