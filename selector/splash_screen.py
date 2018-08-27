04bca4036017c84749b77bf5f4229527dfd2a925
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-27 13:52:09 -0500

    splash_screen now has a label that changes to the name of the directory or video that is selected to be encoded! :-)

--- selector/splash_screen.py
+++ selector/splash_screen.py
@@ -12,12 +12,14 @@ from tkinter.filedialog import askopenfilename, askdirectory # will need later o
 from selection import directory, video
 # TODO(5): learn how to create a SplashScreen OOP class
 
+
 # TODO(6): if button is pressed, open a new window for encoding that has the folder name
 # returns directory selected from file dialog
 def directory_button_command():
     path_to_directory = askdirectory(initialdir='W:\Brandon J. Fletcher\Videos')
     chosen_directory = directory.Directory(path_to_directory)
     print(path_to_directory)
+    name_of_file_or_directory_label.config(text=chosen_directory.name())
     return chosen_directory
 
 
@@ -28,6 +30,7 @@ def video_button_command():
                                     ('AVI', '*.avi'), ('MKV', '*.mkv')))
     chosen_video = video.Video(path_to_video)
     print(path_to_video)
+    name_of_file_or_directory_label.config(text=chosen_video.title())
     return chosen_video
 
 
@@ -38,10 +41,19 @@ root.title('Fletcher Video Encoder')
 
 # add GUI components
 
-directory_button = Button(root, text='Directory', command=directory_button_command)
+label_frame = Frame(root)
+label_frame.pack(side=TOP)
+
+buttons_frame = Frame(root)
+buttons_frame.pack(side=BOTTOM)
+
+name_of_file_or_directory_label = Label(label_frame)
+name_of_file_or_directory_label.pack()
+
+directory_button = Button(buttons_frame, text='Directory', command=directory_button_command)
 directory_button.pack(side=LEFT, expand=YES)
 
-video_button = Button(root, text='Video', command=video_button_command)
+video_button = Button(buttons_frame, text='Video', command=video_button_command)
 video_button.pack(side=RIGHT, expand=YES)
 
 root.mainloop()
