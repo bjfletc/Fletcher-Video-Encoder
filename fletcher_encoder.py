039ae3979287cf935cf4ac1f9ed35b81d9356e0b
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-09-08 12:12:56 -0500

    Cleaned up visually.

--- fletcher_encoder.py
+++ fletcher_encoder.py
@@ -8,7 +8,6 @@
 import threading
 import subprocess
 import sys
-import ctypes
 sys.path.append('W:\Brandon J. Fletcher\Computer Programming\Code\Python\Fletcher Video Encoder')
 
 
@@ -22,9 +21,9 @@ class Encoder:
         # test multiprocessing.fork()
         # global ffmpeg_pipe  #added for test0
         self.ffmpeg_pipe = subprocess.Popen(cmd, stdin=subprocess.PIPE,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE,
-                                       universal_newlines=True)
+                                            stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE,
+                                            universal_newlines=True)
 
         ferr = self.ffmpeg_pipe.stderr
         for line in ferr:
@@ -35,17 +34,14 @@ class Encoder:
         if self.ffmpeg_pipe.returncode != 0:
             raise subprocess.CalledProcessError(self.ffmpeg_pipe.returncode, cmd)
 
-
     def start_subprocess_thread(self, going_to_encode):
         t = threading.Thread(target=self.start_subprocess, args=(going_to_encode,))
         t.daemon = True
         t.start()
         return t
 
-
     def stop_subprocess_thread(self):
         self.ffmpeg_pipe.terminate()
-        #ctypes.windll.kernel32.TerminateProcess(int(self.ffmpeg_pipe._handle), -1)
         return print('Subprocess Terminated...')
 
 
