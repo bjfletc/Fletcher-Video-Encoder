4dc98c21a5bbe5f6f2cb8950e980ed606e93c8b0
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-09-07 19:01:59 -0500

    Going to wrap all encoder modules into a class.

---
+++ encoder/encode.py
@@ -0,0 +1,14 @@
+# Author: Brandon J. Fletcher
+# Created  : Friday, Sep 07, 2018
+# E-mail: brandonjfletcher@gmail.com
+# GitHub: https://github.com/bjfletc
+
+"""encode.py is going to be the main module for encoding videos."""
+
+# TODO(0) Need to Wrap All Encoding Processes Into Single Class
+
+print('New Module...')
+
+
+if __name__ == '__main__':
+    print('Going to be my new module...')
