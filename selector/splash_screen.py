d6b7f42cb3e69909e9d1f6864c92cb66e31595f2
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-09-09 15:58:56 -0500

    removing un-needing .append()

--- selector/splash_screen.py
+++ selector/splash_screen.py
@@ -9,8 +9,6 @@ import os
 path_to_append = os.getcwd()
 path_to_append = path_to_append[0:path_to_append.rfind('\\')]
 sys.path.append(path_to_append)
-print(path_to_append)
-#sys.path.append('W:\Brandon J. Fletcher\Computer Programming\Code\Python\Fletcher Video Encoder')
 from tkinter import *
 from tkinter.filedialog import askopenfilename, askdirectory # will need later on
 from selection import directory, video
